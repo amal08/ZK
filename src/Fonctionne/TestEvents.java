a763a594ea1c8dfa8ed81569f38baa5459ed574d
Ghassen Khalil Ati <ghassen.ati@sifast.com>
2016-07-12 18:03:42 +0100

    onuser event

--- src/Fonctionne/TestEvents.java
+++ src/Fonctionne/TestEvents.java
@@ -8,30 +8,42 @@ public class TestEvents extends ActiveXInvocationProxy {
 
     static Variant machineNumber = new Variant(1, true);
 
-    Variant dwEnrollNumber = new Variant(900, true);
+    Variant tmpData2 = new Variant();
 
+    Variant Size = new Variant();
+
+    Variant dwEnrollNumber = new Variant(900, true);
+boolean deleted = false;
+Variant FingerTmp = new Variant("", true);
     @Override
     public Variant invoke(String methodName, Variant[] targetParameters) {
         System.out.println("Received event from  : " + methodName + "\n");
+       
+        Variant fingerLength = new Variant(0, true);
         if (methodName.equals("OnEnrollFinger")) {
             Dispatch.call(IService.INSTANCE, "RefreshData", machineNumber);
             Variant fingerIndex = new Variant(6, true);
             byte b = 0;
-            Variant FingerTmp = new Variant("", true);
-            Variant fingerLength = new Variant(0, true);
+
             if (Dispatch.call(IService.INSTANCE, "SSR_GetUserTmpStr", machineNumber, dwEnrollNumber, fingerIndex, FingerTmp, fingerLength).getBoolean()) {
                 System.out.println("fingerLength " + fingerLength);
                 System.out.println("FingerTmp: " + FingerTmp);
 
-                // delete tmp user
-                Dispatch.call(IService.INSTANCE, "SSR_DeleteEnrollDataExt", machineNumber, dwEnrollNumber, 12);
+                deleted = true;
 
+            }
+        }
+        if (methodName.equals("OnNewUser")&& deleted) {
+            // delete tmp user
+            if (Dispatch.call(IService.INSTANCE, "SSR_DeleteEnrollDataExt", machineNumber, dwEnrollNumber, 12).getBoolean()) {
                 // create final user
                 System.out.println(Dispatch.call(IService.INSTANCE, "SSR_SetUserInfo", machineNumber, dwEnrollNumber, new Variant("finalUser", true), new Variant("", true),
                         new Variant(0, true), new Variant(true, true)).getBoolean());
-                System.out.println(Dispatch.call(IService.INSTANCE, "SetUserTmp", machineNumber, dwEnrollNumber, new Variant(6, true), FingerTmp.getString()).getBoolean());
+                System.out.println(FingerTmp);
+                 System.out.println(Dispatch.call(IService.INSTANCE, "SetUserTmpExStr", machineNumber, dwEnrollNumber, new Variant(6, true),FingerTmp).getBoolean());
                 Dispatch.call(IService.INSTANCE, "RefreshData", machineNumber);
             }
+            deleted =false;
         }
         return null;
     }
