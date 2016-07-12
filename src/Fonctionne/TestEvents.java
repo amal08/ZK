087439c764e4435ff5b3df7488a69e0de1ef23e1
Amal Jerbi <amal.jerbi@sifast.com>
2016-07-12 16:52:05 +0100

    add final user

--- src/Fonctionne/TestEvents.java
+++ src/Fonctionne/TestEvents.java
@@ -19,10 +19,19 @@ public class TestEvents extends ActiveXInvocationProxy {
             byte b = 0;
             Variant FingerTmp = new Variant("", true);
             Variant fingerLength = new Variant(0, true);
+            if (Dispatch.call(IService.INSTANCE, "SSR_GetUserTmpStr", machineNumber, dwEnrollNumber, fingerIndex, FingerTmp, fingerLength).getBoolean()) {
+                System.out.println("fingerLength " + fingerLength);
+                System.out.println("FingerTmp: " + FingerTmp);
 
-            Dispatch.call(IService.INSTANCE, "SSR_GetUserTmpStr", machineNumber, dwEnrollNumber, fingerIndex, FingerTmp, fingerLength).getBoolean();
-            System.out.println("fingerLength " + fingerLength);
-            System.out.println("FingerTmp: " + FingerTmp);
+                // delete tmp user
+                Dispatch.call(IService.INSTANCE, "SSR_DeleteEnrollDataExt", machineNumber, dwEnrollNumber, 12);
+
+                // create final user
+                System.out.println(Dispatch.call(IService.INSTANCE, "SSR_SetUserInfo", machineNumber, dwEnrollNumber, new Variant("finalUser", true), new Variant("", true),
+                        new Variant(0, true), new Variant(true, true)).getBoolean());
+                System.out.println(Dispatch.call(IService.INSTANCE, "SetUserTmp", machineNumber, dwEnrollNumber, new Variant(6, true), FingerTmp.getString()).getBoolean());
+                Dispatch.call(IService.INSTANCE, "RefreshData", machineNumber);
+            }
         }
         return null;
     }
